79e8709e29ca44e7ce53306bb65588f9b3add3ae
Yousif Wali <wali.yousiff@gmail.com>
2022-06-08 09:13:30 -0500

    React

---
+++ src/Header.js
@@ -0,0 +1,18 @@
+import './App.css';
+
+function Header() {
+  return (
+    <header>
+        <nav>
+            <ul>
+                <li>Home</li>
+                <li>Shop</li>
+                <li>Contact</li>
+                
+            </ul>
+        </nav>
+    </header>
+  );
+}
+
+export default Header;
