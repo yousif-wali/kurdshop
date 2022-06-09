50e07de33c641c778e0640fd35310e16ffcc9bed
Yousif Wali <wali.yousiff@gmail.com>
2022-06-09 09:47:27 -0500

    Adding server

---
+++ kurdshop/src/Header.js
@@ -0,0 +1,20 @@
+
+function Header() {
+  return (
+    <header>
+        <nav>
+            <ul>
+                <li>Home</li>
+                <li>Shop</li>
+                <li>mobile</li>
+                <li>otombel</li>
+                <li>dilivery</li>
+                <li>Contact</li>
+                <li>About us</li>
+            </ul>
+        </nav>
+    </header>
+  );
+}
+
+export default Header;
