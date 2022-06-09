d13af4daa0e577e4115a3ae713df8fb6fb0e586e
Yousif Wali <wali.yousiff@gmail.com>
2022-06-09 10:14:14 -0500

    server updated

--- server/index.js
+++ server/index.js
@@ -1,6 +1,18 @@
 const express = require("express")
 const app = express()
+const mysql = require("mysql")
+
+const db = mysql.createPool({
+    host: "localhost",
+    user: "root",
+    password: "",
+    database: "kurdshop"
+});
+
+app.get('/', (req, res)=>{
+   
+})
 
 app.listen(3001, ()=>{
     console.log("running")
-})
+});
