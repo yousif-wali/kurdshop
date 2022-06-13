d1d94b7879b6a867095bc22b2979abfc38a277e9
sardarwalikhano <sardar.xano59@gmail.com>
2022-06-13 22:25:47 +0300

    update

--- kurdshop/src/App.js
+++ kurdshop/src/App.js
@@ -2,8 +2,13 @@
 
 function App() {
   return (
-    <div className="container row">
-      <span className="col-lg-5 offset-0 col-md-5 col-sm-12">TextArea</span>
+    <div className="container-fluid row">
+      <span className="col-lg-5 offset-0 col-md-5 col-sm-12">
+        <h3>Title</h3>
+        <p>This is title</p>
+        </span>
+      <span className="col-lg-7 offset-0 col-md-7 col-sm-12">Cards</span>
+      <span className="col-lg-7 offset-0 col-md-7 col-sm-12">Cards</span>
       <span className="col-lg-7 offset-0 col-md-7 col-sm-12">Cards</span>
     </div>
   );
--- kurdshop/src/Header.js
+++ kurdshop/src/Header.js
@@ -2,14 +2,14 @@
 function Header() {
   return (
     <header>
-        <div id='logo'>Logo</div>
+        <div id='logo'>Kurdshop</div>
         <div id='navigation'>
           <ul className="nav nav-tabs">
-          <li class="nav-item"><a class="nav-link active" data-bs-toggle="tab" href="#home">Home</a></li>
-          <li class="nav-item"><a class="nav-link" data-bs-toggle="tab" href="#shop">Shop</a></li>
-          <li class="nav-item"><a class="nav-link" data-bs-toggle="tab" href="#mobile">Mobile</a></li>
-          <li class="nav-item"><a class="nav-link" data-bs-toggle="tab" href="#religion">Religion</a></li>
-          <li class="nav-item"><a class="nav-link" data-bs-toggle="tab" href="#reklam">Reklam</a></li>
+          <li className="nav-item"><a className="nav-link active" data-bs-toggle="tab" href="#home">Home</a></li>
+          <li className="nav-item"><a className="nav-link" data-bs-toggle="tab" href="#shop">Shop</a></li>
+          <li className="nav-item"><a className="nav-link" data-bs-toggle="tab" href="#mobile">Mobile</a></li>
+          <li className="nav-item"><a className="nav-link" data-bs-toggle="tab" href="#religion">Religion</a></li>
+          <li className="nav-item"><a className="nav-link" data-bs-toggle="tab" href="#reklam">Reklam</a></li>
           </ul>          
         </div>
         <div id='user'>Login</div>
