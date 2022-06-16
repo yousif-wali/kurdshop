47544325cbdc4667a91afd59d936cb6432bd4ce1
sardarwalikhano <sardar.xano59@gmail.com>
2022-06-16 17:13:37 +0300

    Change logo title

--- kurdshop/src/Header.js
+++ kurdshop/src/Header.js
@@ -11,14 +11,14 @@ function Header() {
  }
   return (
     <header>
-        <div id='logo'>Kurdshop</div>
+        <div id='logo'> Kurd Mega</div> 
         <div id='navigation'>
           <span id="menu" onClick={menu}>{bars}</span>
           <ul id="nav" className="nav nav-tabs">
           <li className="nav-item"><a className="nav-link active" data-bs-toggle="tab" href="#home">Home</a></li>
           <li className="nav-item"><a className="nav-link" data-bs-toggle="tab" href="#shop">Shop</a></li>
           <li className="nav-item"><a className="nav-link" data-bs-toggle="tab" href="#mobile">Mobile</a></li>
-          <li className="nav-item"><a className="nav-link" data-bs-toggle="tab" href="#religion">Religion</a></li>
+          <li className="nav-item"><a className="nav-link" data-bs-toggle="tab" href="#religion">Uplode </a></li>
           <li className="nav-item"><a className="nav-link" data-bs-toggle="tab" href="#reklam">Reklam</a></li>
           <li className="nav-item"><a className="nav-link" href="#login">Login</a></li>
           </ul>   
