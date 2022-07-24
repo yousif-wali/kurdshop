b1aef27900b5e9815034cb80398d2f055e24534f
Yousif Wali <wali.yousiff@gmail.com>
2022-07-24 15:30:01 -0500

    Shop design

---
+++ kurdshop/src/Data/Categories.js
@@ -0,0 +1,10 @@
+export const categories = [
+    {"id": 1, "category": "ئەلیکتۆرنی", "sublist":[
+        {"id": 1, "category":"موبایل"},
+        {"id": 2, "category":"کۆمپیوتەر"}
+    ]},
+    {"id": 2, "category": "کەل و پەلی ناو ماڵ"},
+    {"id": 3, "category": "ئوتۆمبێل"},
+    {"id": 4, "category": "جل و بەرگ"},
+    {"id": 5, "category": "ئاژەڵ"}
+]
--- kurdshop/src/page/Shop.js
+++ kurdshop/src/page/Shop.js
@@ -1,9 +1,49 @@
-import ShopPosts from './../Data/ShopPosts'
+import {categories} from "./../Data/Categories"
+function Sublist(){
+   return categories[0].sublist.map((list, index)=>{
+      return(
+        <li key={index}>{list.category}</li>
+      )
+    })
+    
+}
 export default function Shop(){
     return (
-      <div className="container-fluid row">
-        Welcome to shop
-        <ShopPosts />
-      </div>
+      <>
+        <section id="Header">
+          <section id="Category"><ul>
+            {
+             // fetch("./Data/Categories.json").then(res=>res.json()).then(res=>{console.log(res)}).catch(e=>{console.error(e.message)})
+             categories.map((res, index)=>{
+            return (
+              
+                <li key={index}>{res.category}
+                {
+                  res.sublist && <ul><Sublist/></ul>
+                }
+                </li>
+           
+             )
+            })
+           } </ul>
+          </section>
+          <section id="Post">
+
+          </section>
+        </section>
+        <section id="Main">
+          <section id="Posts">
+
+          </section>
+          <section id="Promotions">
+            <section id="Vip">
+
+            </section>
+            <section id="Ad">
+
+            </section>
+          </section>
+        </section>
+      </>
       );
 }
