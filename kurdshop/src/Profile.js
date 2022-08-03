615d5eaa4d8ce8920b5a3929fd5a6d22b3b0440d
Yousif Wali <wali.yousiff@gmail.com>
2022-08-02 22:54:29 -0500

    user profile

--- kurdshop/src/Profile.js
+++ kurdshop/src/Profile.js
@@ -5,7 +5,7 @@ const Profile = () => {
   const { user, isAuthenticated, isLoading } = useAuth0();
   console.log(user)
   return (
-    isAuthenticated && (
+    isAuthenticated && !isLoading && (
       <div>
         <img src={user.picture} alt={user.name}/>
         <h2>{user.name}</h2>
